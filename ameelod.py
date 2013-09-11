a57c6a037d053230e71a9498fbbcd90f7a12dc7a
Rafael Ramos <rrasmo@gmail.com>
2013-09-11 17:40:09 +0100

    Search companies by name on amee api

--- ameelod.py
+++ ameelod.py
@@ -1,5 +1,12 @@
 from flask import Flask
 from flask import render_template, url_for, request
+import requests
+
+
+API_KEY = ''
+API_SECRET = ''
+API_COMPANIES_URL = 'https://www.amee.com/api/companies'
+API_COMPANY_URL = 'https://www.amee.com/api/company'
 
 
 app = Flask(__name__, static_url_path='')
@@ -7,8 +14,17 @@ app = Flask(__name__, static_url_path='')
 @app.route('/')
 def index():
     q = request.args.get('q')
-    print q
-    return render_template('index.html', q=q)
+    companies = None
+    if q:
+        res = requests.get(API_COMPANIES_URL, auth=(API_KEY, API_SECRET), params={'company_name': q})
+        if res.ok:
+            companies = res.json()['companies']
+    return render_template('index.html', companies=companies)
+
+
+@app.route('/company/<cro>')
+def company(cro):
+    return cro
 
 
 if __name__ == '__main__':
